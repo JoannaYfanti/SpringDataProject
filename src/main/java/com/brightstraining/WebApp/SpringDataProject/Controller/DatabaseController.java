c717338a2b56ddbdce497d73a889ba0a910a551b
Ioanna Yfanti <Ioanna.Yfanti@awacademy.traning>
2022-11-09 20:08:39 +0200

    delete/update student

--- src/main/java/com/brightstraining/WebApp/SpringDataProject/Controller/DatabaseController.java
+++ src/main/java/com/brightstraining/WebApp/SpringDataProject/Controller/DatabaseController.java
@@ -28,21 +28,24 @@ public class DatabaseController {
     @GetMapping("/")
     @ResponseBody
     public ResponseEntity<String> addNewStudentToTheDatabase() {
-//        Student student = new Student();
-//        student.setName("Ioanna");
-//        student.setAge(36);
-//        student.setLastName("Yfanti");
-//        student.setEmail("i.yfanti@cosmote-evalue.gr");
-//
-//        studentRepository.save(student);
-//
-//        return ResponseEntity.ok("Save " + student.getName() +" " +student.getLastName() + " " + student.getAge()+ " " +student.getEmail() + " to database.");
+
+        String listOfStudents = "<select name=\"listOfStudents\">";
+        List<Student> students = (List<Student>) studentRepository.findAll();
+        for (Student newStudents:students) {
+            listOfStudents += "<option>" + newStudents.getName() + " " + newStudents.getLastName() +  newStudents.getAge() + newStudents.getEmail() + "</option>";
+        }
+        listOfStudents += "</select>";
+
+
         return ResponseEntity.ok("<form action=\"/added\" method=\"POST\">" +
                 "<input name=\"name\" placeholder=\"Student's first name\">" +
                 "<input name=\"lastName\" placeholder=\"Student's last name\">" +
                 "<input name=\"age\" placeholder=\"Student's age\">" +
                 "<input name=\"email\" placeholder=\"Student's email address\">" +
                 "<button>Add</button>" +
+                listOfStudents+
+                "<button formaction=\"/update\" method=\"POST\">Update</button>" +
+                "<button formaction=\"/delete\" method=\"POST\">Remove</button>" +
                 "</form>");
     }
 
@@ -57,6 +60,61 @@ public class DatabaseController {
         }
         return ResponseEntity.ok(studentAttributes + "</dl>");
     }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> removeStudent(@RequestParam String listOfStudents) {
+//        return ResponseEntity.ok(addToDBService.addNewStudent(name, lastName, age, email, studentRepository));
+
+        List<Student> students = (List<Student>) studentRepository.findAll();
+        for (Student student:students) {
+            String st = student.getName() + " " + student.getLastName()+ student.getAge()+ student.getEmail();
+            if(st.equals(listOfStudents)){
+                studentRepository.delete(student);
+                break;
+            }
+        }
+        return ResponseEntity.ok("Student deleted");
+    }
+    @PostMapping("/update")
+    public ResponseEntity <String> updateStudent (@RequestParam String listOfStudents){
+
+        List<Student> students = (List<Student>) studentRepository.findAll();
+        Student studentToUpdate = null;
+        for (Student student:students) {
+            String st = student.getName() + " " + student.getLastName()+ student.getAge()+ student.getEmail();
+            if(st.equals(listOfStudents)){
+                studentToUpdate= student;
+                break;
+            }
+
+            }
+
+        return ResponseEntity.ok("<form action=\"/updated\" method=\"GET\">" +
+                "<input name=\"name\" value= " + studentToUpdate.getName()+">" +
+                "<input name=\"lastName\" value= " + studentToUpdate.getLastName()+ ">" +
+                "<input name=\"age\" value=" + studentToUpdate.getAge()+ ">"+
+                "<input name=\"email\" value=" + studentToUpdate.getEmail()+ ">"+
+                "<input name=\"id\" disabled=\"disabled\" value=" + studentToUpdate.getId()+ ">"+
+                "<button>Save</button>" +
+                "</form>");
+
+    }
+    @GetMapping("/updated")
+    @ResponseBody
+    public ResponseEntity <String> modifyStudent (@RequestParam Long id, @RequestParam String name, @RequestParam String lastName, @RequestParam String age, @RequestParam String email){
+        Student studentToModify = (Student) studentRepository.findById(id).get();
+
+        studentToModify.setName(name);
+        studentToModify.setLastName(lastName);
+        studentToModify.setAge(Integer.parseInt(age));
+        studentToModify.setEmail(email);
+
+        studentRepository.save(studentToModify);
+
+
+        return ResponseEntity.ok(studentToModify.getName());
+    }
+
 
 
 }
